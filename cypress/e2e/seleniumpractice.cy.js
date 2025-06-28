d2966896a013e94b9423a57dce64665e43b4a8c0
KushalSharma28 <kushalsharma28499@gmail.com>
2025-06-28 10:09:26 +0530

    New Changes

--- cypress/e2e/seleniumpractice.cy.js
+++ cypress/e2e/seleniumpractice.cy.js
@@ -26,6 +26,10 @@ describe('My First Test Suite', function()
         cy.contains('PROCEED TO CHECKOUT').click()
         cy.contains('Place Order').click()
 
+        cy.get('select').select("India")
+        cy.get('.chkAgree').check();
+        cy.get('button').contains('Proceed').click();
+
         // cy.get('.brand').then(function(logoelement)
         // {
         //     cy.log(logoelement.text())
