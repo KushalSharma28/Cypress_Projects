f44a5d93e58a0544c59310fd007650ca0437ba6b
KushalSharma28 <kushalsharma28499@gmail.com>
2025-08-31 21:52:23 +0530

    Script updated

--- cypress/e2e/tricentisdemosite.cy.js
+++ cypress/e2e/tricentisdemosite.cy.js
@@ -4,14 +4,28 @@ describe('Tricentis Demo Test Suite', function()
     {
         //navigate to url
         cy.visit("https://demowebshop.tricentis.com")
+        
         //click on register button
-        cy.get('.ico-register').click()
+        //cy.get('.ico-register').click()
 
         //fill the form
-        cy.get('#gender').should('be.visible').click()
-        cy.get('#FirstName').should('be.visible').type('Kushal')
-        cy.get('#LastName').should('be.visible').type('Sharma')
+        // cy.get('#gender-male').click();
+        // cy.get('#FirstName').should('be.visible').type('Kushal')
+        // cy.get('#LastName').should('be.visible').type('Sharma')
+        // cy.get('#Email').should('be.visible').type('kushalsharmatester@gmail.com')
+        // cy.get('#Password').should('be.visible').type('kushal@123')
+        // cy.get('#ConfirmPassword').should('be.visible').type('kushal@123')
+        // cy.get('#register-button').should('be.visible').click()
+
+        //click on login button
+        cy.get('.ico-login').click()
+
+        //login to the application
         cy.get('#Email').should('be.visible').type('kushalsharmatester@gmail.com')
-        
+        cy.get('#Password').should('be.visible').type('kushal@123')
+        cy.get('form').contains('Log in').click()
+
+        //logout from the application
+        cy.get('.ico-logout').click()
     })
 })
