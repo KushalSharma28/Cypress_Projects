14406a4921921329101cc85294b3ce5ab16970d1
KushalSharma28 <kushalsharma28499@gmail.com>
2025-11-17 10:44:24 +0530

    Configure File Update for Pipeline Build

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    reporter: "junit",
+    reporterOptions: {
+      mochaFile: "cypress/results/results-[hash].xml",
+      toConsole: true
+    }
+    //setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    //},
   },
 });
